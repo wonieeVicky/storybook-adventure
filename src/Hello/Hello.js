bb5e6b2bba6b6e6a44a83f99df05d9be1d2431e1
Vicky <hwfongfing@gmail.com>
2020-10-14 22:58:55 +0900

    feat: actions addon 적용

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Hello = ({ name, big, small }) => {
-  if (big) {
-    return <h1>안녕하세요, {name}!</h1>;
-  }
-  if (small) {
-    return <small>안녕하세요, {name}</small>;
-  }
-  return <p>안녕하세요, {name}!</p>;
+const Hello = ({ name, big, onHello, onBye }) => {
+  return (
+    <div>
+      {big ? <h1>안녕하세요, {name}!</h1> : <p>안녕하세요, {name}!</p>}
+      <div>
+        <button onClick={onHello}>Hello</button>
+        <button onClick={onBye}>Bye</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hello;
